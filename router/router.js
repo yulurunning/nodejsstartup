bbcbf3e2af3b7550f99111389ee4c21c370dee6f
yuluxurunning <yuluxu@outlook.com>
2017-08-13 20:30:18 +0800

    add router demo

---
+++ router/router.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var url = require('url');
+
+function start() {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log('Request for ' + pathname + ' received');
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('hello world');
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server started');
+}
+
+function route(pathname) {
+  console.log('About to route a request for', pathname);
+}
+
+exports.start = start;
+exports.route = route;
