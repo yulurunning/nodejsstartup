35d1bd734412a39f9c0d8559850eb84b8331be9b
yuluxurunning <yuluxu@outlook.com>
2017-08-13 20:06:40 +0800

    add server demo

---
+++ server/app.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var fs = require('fs');
+
+var data = '';
+var readerStream = fs.createReadStream('input.txt');
+readerStream.setEncoding('UTF8');
+readerStream.on('data', function(chunk){
+  data += chunk;
+});
+
+readerStream.on('end', function(){
+  console.log('read file end');
+  console.log('content is', data);
+});
+
+readerStream.on('error', function(err){
+  console.log('read file failed', err.stack);
+});
+
+var onRequest = function(request, response){
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write(data);
+  response.end();
+};
+http.createServer(onRequest).listen(8888);
