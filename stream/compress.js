a8ed11f6c43aa862ac0a25b3688bd8c9fb0e3cb9
yuluxurunning <yuluxu@outlook.com>
2017-08-13 17:44:50 +0800

    添加链式流处理demo

---
+++ stream/compress.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var zlib = require('zlib');
+
+fs.createReadStream('input.txt')
+  .pipe(zlib.createGzip())
+  .pipe(fs.createWriteStream('output.txt.gz'));
+
+console.log('App exit');
